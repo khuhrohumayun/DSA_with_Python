006292dedd029d4dbcc70eca2c074597ca17a1cf
Muhammad Humayun <humayunmuhammad902@gmail.com>
2025-10-26 23:19:34 +0500

    Insertion Sort..

---
+++ insertion_sort.py
@@ -0,0 +1,19 @@
+
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i -1 
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+
+
+arr = input("Enter numbers separated by spaces: ").split()
+arr = [int(x) for x in arr]
+
+insertion_sort(arr)
+print("Sorted array is:", arr)
